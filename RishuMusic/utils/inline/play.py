73bdee222710e8533ebb24445ee1e6bcbd228415
Prin718 <sakarsandhya1987@gmail.com>
2025-01-11 22:43:06 +0530

    Update play.py

--- RishuMusic/utils/inline/play.py
+++ RishuMusic/utils/inline/play.py
@@ -75,7 +75,7 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", user_id=config.OWNER_ID),,
+         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", user_id=config.OWNER_ID),
          InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url=f"{SUPPORT_CHAT}",),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
