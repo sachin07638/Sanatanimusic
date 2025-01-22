8b3112dccf8091fc9d11e31347087592dd99cd17
Prin718 <sakarsandhya1987@gmail.com>
2025-01-22 17:53:07 +0530

    Update approve.py

--- RishuMusic/plugins/bot/approve.py
+++ RishuMusic/plugins/bot/approve.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Rishu_musicbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/sanataniimusicbot?startgroup=true"),
     ],
 ]
 
