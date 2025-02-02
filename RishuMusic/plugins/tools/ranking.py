2f331d51cd12d55361253f991b31b8b3a5d61205
Prin718 <sakarsandhya1987@gmail.com>
2025-02-03 00:18:53 +0530

    Update ranking.py

--- RishuMusic/plugins/tools/ranking.py
+++ RishuMusic/plugins/tools/ranking.py
@@ -57,7 +57,7 @@ MISHI = [
 @app.on_message(filters.group & filters.group, group=6)
 def today_watcher(_, message):
     chat_id = message.chat.id
-    user_id = message.from_user.mention
+    user_id = message.from_user.id
     if chat_id in today and user_id in today[chat_id]:
         today[chat_id][user_id]["total_messages"] += 1
     else:
@@ -72,7 +72,7 @@ def today_watcher(_, message):
 @app.on_message(filters.group & filters.group, group=6)
 def today_watcher(_, message):
     chat_id = message.chat.id
-    user_id = message.from_user.mention
+    user_id = message.from_user.id
     if chat_id in today and user_id in today[chat_id]:
         today[chat_id][user_id]["total_messages"] += 1
     else:
@@ -86,7 +86,7 @@ def today_watcher(_, message):
 
 @app.on_message(filters.group & filters.group, group=11)
 def _watcher(_, message):
-    user_id = message.from_user.mention    
+    user_id = message.from_user.id    
     user_data.setdefault(user_id, {}).setdefault("total_messages", 0)
     user_data[user_id]["total_messages"] += 1    
     collection.update_one({"_id": user_id}, {"$inc": {"total_messages": 1}}, upsert=True)
